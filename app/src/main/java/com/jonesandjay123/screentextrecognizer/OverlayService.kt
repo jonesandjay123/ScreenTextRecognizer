5543a49140c4e4eeda108d9c54e0025fef71b19a
joneswang <jonescwang@gmail.com>
2023-11-18 13:58:37 -0500

    red border effect is done

--- app/src/main/java/com/jonesandjay123/screentextrecognizer/OverlayService.kt
+++ app/src/main/java/com/jonesandjay123/screentextrecognizer/OverlayService.kt
@@ -3,12 +3,9 @@ package com.jonesandjay123.screentextrecognizer
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import android.view.LayoutInflater
-import android.view.WindowManager
-import android.view.Gravity
 import android.graphics.PixelFormat
 import android.util.Log
-import android.view.View
+import android.view.*
 import android.widget.Button
 
 class OverlayService : Service() {
@@ -39,9 +36,43 @@ class OverlayService : Service() {
         params.gravity = Gravity.CENTER
         windowManager.addView(overlayView, params)
 
+        // Initialize variables for tracking touch position and view position
+        var initialX = 0
+        var initialY = 0
+        var initialTouchX = 0f
+        var initialTouchY = 0f
+
+        // Find the red box view
+        val redBox = overlayView.findViewById<View>(R.id.redBox)
+
+        // Set touch listener for moving the overlay view
+        redBox.setOnTouchListener { view, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // Remember the initial position when the touch started
+                    initialX = params.x
+                    initialY = params.y
+                    initialTouchX = event.rawX
+                    initialTouchY = event.rawY
+                    true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    // Calculate the X and Y coordinates of the view
+                    params.x = initialX + (event.rawX - initialTouchX).toInt()
+                    params.y = initialY + (event.rawY - initialTouchY).toInt()
+
+                    // Update the layout with new X & Y coordinates
+                    windowManager.updateViewLayout(overlayView, params)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Handling the close button click
         val closeButton = overlayView.findViewById<Button>(R.id.closeButton)
         closeButton.setOnClickListener {
-            stopSelf() // 停止服務
+            stopSelf() // Stop the service
         }
     }
 
